d6bca96b46c639191b5a270ab1443c5c051d739a
Pranjal Kumar <115923450+Pranjal-Kumar1@users.noreply.github.com>
2022-11-07 06:22:15 +0530

    Delete HelloWorld.java

--- Basics/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.print("Hello Java\n");
-        System.out.print("Hello World with Java\n");
-
-    }
-}
