1aac4d91f09709b8f6bedfa354d3741d9aae7e0e
Pranjal Kumar <115923450+Pranjal-Kumar1@users.noreply.github.com>
2022-11-13 23:30:44 +0530

    Create ChangeVariable.java

---
+++ Basics/ChangeVariable.java
@@ -0,0 +1,10 @@
+public class ChangeVariable {
+    public static void main(String[] args) {
+        int a = 100;
+        String s = "Atul";
+        System.out.println(s+ " "+a);
+        a = 1120;
+        s = "Java";
+        System.out.println(s+ " " +a);
+    }
+}
