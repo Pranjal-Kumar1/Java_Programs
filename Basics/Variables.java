da230d2f12cd228e9bb602870e04de0ec3163a41
Pranjal Kumar <115923450+Pranjal-Kumar1@users.noreply.github.com>
2022-11-13 23:14:29 +0530

    Create Variables.java

---
+++ Basics/Variables.java
@@ -0,0 +1,21 @@
+public class Variables{
+    public static void main(String[] args) {
+        byte b = 12;
+        short sh = 11211;
+        int i = 12413112;
+        long l = 1311231323;
+        float f = 111.22f;
+        double d = 12122312.391;
+        char ch = 'P';
+        boolean flag= true;
+
+        System.out.println(b);
+        System.out.println(sh);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(flag);
+    }
+}
